f192e1bb0ec99e7bbf93d07e52477b53addf5bae
DmitriPrijomosev <dmitri@dezajno.ee>
2022-08-16 22:04:08 +0300

    25. Coop API
    COOP API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [products, setProducts] = useState([]); 
+  const productsDb = "https://api.saaremaa.ecoop.ee/supermarket/products?language=et&page=1&orderby=popularity";
+
+  useEffect(() => {
+    fetch(productsDb)
+      .then(res => res.json())
+      .then(json => {
+        setProducts(json.data);
+      })
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>Tooteid on {products.length} tükki</div>
+
+        
+          {products.map(element =>
+            <div className='section' key={element.id}>
+              <img src={element.image}></img>
+              <div className='name'>{element.name}</div>
+              <div className='producer'>Tootja/päritolumaa: {element.producer}</div>
+              <div className='price'>Hind: {element.price}</div>
+    
+            </div>
+        
+
+      )}
     </div>
   );
 }
